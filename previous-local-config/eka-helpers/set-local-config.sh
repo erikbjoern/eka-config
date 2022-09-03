0f8e81d3da2c7c56f5fed62eb5fbeee30996b7bf
Erik Björn <erik.g.bjoern@gmail.com>
2022-09-03 18:24:30 +0200

    remove previous-local-config from git

--- previous-local-config/eka-helpers/set-local-config.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p previous-local-config
-cp -f $HOME/.gitconfig $HOME/eka-config/previous-local-config/
-cp -f $HOME/.bash_profile $HOME/eka-config/previous-local-config/
-cp -rf $HOME/eka-helpers $HOME/eka-config/previous-local-config/
-
-cp -f $HOME/eka-config/.gitconfig $HOME/
-cp -f $HOME/eka-config/.bash_profile $HOME/
-cp -rf $HOME/eka-config/eka-helpers $HOME/
